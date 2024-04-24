a81ffbacaa7b68d1256f228ea043d9adb02b98e8
hiroki185 <68848005+hiroki185@users.noreply.github.com>
2024-04-24 11:58:29 +0000

    投稿一覧のレイアウトのアニメーションの作成完了

--- app/controllers/tagsearches_controller.rb
+++ app/controllers/tagsearches_controller.rb
@@ -2,7 +2,7 @@ class TagsearchesController < ApplicationController
   def search
     @model = Travel  #search機能とは関係なし
     @word = params[:content]
-    @travel_search = Travel.where("category LIKE?","%#{@word}%").page(params[:page]).per(5)
+    @travel_search = Travel.where("category LIKE?","%#{@word}%").page(params[:page]).per(50)
     @travels = Travel.all.page(params[:page]).per(50)
     @categories = Travel.unique_categories
     @travel_detail  = Travel.joins(:view_counts).group(:id).order('COUNT(view_counts.id) DESC').page(params[:page]).per(50).limit(4)
