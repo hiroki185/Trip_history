8bbd0ca2e6b13075b127ee8108715c6dcf294395
hiroki185 <68848005+hiroki185@users.noreply.github.com>
2024-05-07 13:17:46 +0000

    お問い合わせのレイアウトを変更したところエラーが発生

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
-    validates :name, presence: true
+  validates :name, presence: true
   validates :email, presence: true
   validates :subject, presence: true
   validates :message, presence: true
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
@@ -46,6 +47,7 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
     authentication: 'plain',
     enable_starttls_auto: true
   }
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
